45799160188c7e9aec5350d6f307f36d13d8d80b
thomazjunior <p.thomaz.junior@gmail.com>
2022-03-30 23:22:28 +0100

    Initialazing chess game

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardGame.Board;
+import boardGame.Position;
+
+public class Program {
+	public static void main(String[] args) {
+		Board board = new Board();
+	}
+}
